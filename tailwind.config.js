7a39ebde77c4d166e42149198ae90fdf17300d85
Nic McPhee <mcphee@morris.umn.edu>
2022-11-21 23:01:59 -0600

    Install and set up TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./templates/**/*.html", "./themes/**/*.html", "./themes/**/*.html"],
+    theme: {},
+    variants: {},
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+};
